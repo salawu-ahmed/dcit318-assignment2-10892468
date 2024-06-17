fbf0e8d4be7fd88b12521963dbe4419b0ec20acc
Ahmed <salawuahmed17@gmail.com>
2024-06-17 23:47:18 +0000

    fix/re-arranged Dog and Cat classes

--- Animal.cs
+++ Animal.cs
@@ -4,20 +4,17 @@ public class Animal {
     public void MakeSound() {
         Console.WriteLine("Some Generic Sound");
     }
-
-    public class Dog : Animal {
-        public override void MakeSound() {
-            Console.WriteLine("Bark");
-        }
-    }
-
-    public class Cat : Animal {
-        public override void MakeSound () {
-            Console.WriteLine("Meow");
-        }
+}
+public class Dog : Animal {
+    public override void MakeSound() {
+        Console.WriteLine("Bark");
     }
 }
-
+public class Cat : Animal {
+    public override void MakeSound () {
+        Console.WriteLine("Meow");
+    }
+}
 
 public class Main 
 {
