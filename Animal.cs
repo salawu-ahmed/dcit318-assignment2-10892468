e48359e81eca3615429ce43051dc3175aa8ea651
Ahmed <salawuahmed17@gmail.com>
2024-06-17 23:45:29 +0000

    Created Main method

--- Animal.cs
+++ Animal.cs
@@ -2,18 +2,34 @@ using System;
 
 public class Animal {
     public void MakeSound() {
-        Console.WriteLine("Some Generic Sound")
+        Console.WriteLine("Some Generic Sound");
     }
 
     public class Dog : Animal {
         public override void MakeSound() {
-            Console.WriteLine("Bark")
+            Console.WriteLine("Bark");
         }
     }
 
     public class Cat : Animal {
         public override void MakeSound () {
-            Console.WriteLine("Meow")
+            Console.WriteLine("Meow");
         }
     }
+}
+
+
+public class Main 
+{
+  public static void Main(string[] args)
+  {
+    Animal animal = new Animal();
+    animal.MakeSound(); 
+
+    Dog dog = new Dog();
+    dog.MakeSound();
+
+    Cat cat = new Cat();
+    cat.MakeSound(); 
+  }
 }
